126ae525aeff49b0b1a03df10c4c3d9a338ac339
Xuan Do <doxuancntp@gmail.com>
2019-06-07 12:30:12 -0400

    Commit.

---
+++ online.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="online-status">
+    <div v-if="online" class="online">
+      <span class="light"></span>
+      <p>A staff member is online and available.</p>
+    </div>
+    <div v-if="!online" class="offline">
+      <span class="light"></span>
+      <p>A staff member is offline and unavailable.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      online: true
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.online-status {
+  background: #eee;
+  border: 1px dashed #ddd;
+  display: inline-block;
+  color: #555;
+}
+p {
+  display: inline-block;
+  font-family: arial;
+  margin: 0;
+  color: #555;
+}
+.light {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+.online .light {
+  background: limegreen;
+}
+.offline .light {
+  background: crimson;
+}
+</style>
