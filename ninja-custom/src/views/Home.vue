cd80ccdfe038b31254501561aec1a2c8215af563
Xuan Do <doxuancntp@gmail.com>
2019-06-14 11:25:10 -0400

    Commit.

--- ninja-custom/src/views/Home.vue
+++ ninja-custom/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <HelloWorld />
+
+
+
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  components: {
+    HelloWorld
   }
+}
 </script>
